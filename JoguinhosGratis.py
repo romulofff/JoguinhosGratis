41f0ced7ad44fb2257b5b5295b229862bac8d7bc
Rômulo Férrer Filho <romulofff@alu.ufc.br>
2019-05-11 19:19:52 -0300

    Initial Commit
    Added the first version of the JoguinhosGratis Telegram Bot.

---
+++ JoguinhosGratis.py
@@ -0,0 +1,54 @@
+'''
+This Telegram Bot was developed to send Game Deals to a Telegram Channel
+The deals are retrieved from subreddit r/gamedeals
+
+Only deals over 60% discount is shared.
+
+Developed by: Rômulo Férrer Filho
+Github: github.com/romulofff
+Telegram: @Romulofff
+'''
+
+#! python3
+# |\
+import praw
+import telegram 
+import os
+
+client_id = os.environ['id']
+client_secret = os.environ['secret']
+user_agent = os.environ['agent']
+username = os.environ['username']
+password = os.environ['password']
+telegram_token = os.environ['token']
+# Connecting to Telegram Bot
+bot = telegram.Bot(token=telegram_token)
+
+# Connecting to Reddit App
+reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent, username=username, password=password)
+subreddit = reddit.subreddit("gamedeals")
+
+while True:
+    new = subreddit.stream.submissions(skip_existing=True)
+    # print(new.title)
+    top = subreddit.new(limit=1)
+
+    # status = bot.send_message(chat_id="@Testesrfff", text="NOVO TESTE INICIANDO", parse_mode=telegram.ParseMode.HTML)
+    INDEX = 0
+    for submission in new:
+        
+        title = submission.title
+        url = submission.url
+        msg = ""
+        if "%" in title:
+            if title[title.find("%")-3:title.find("%")+1] == "100%":
+                msg = "*------------ESSE É GRÁTIS------------* \n"
+                msg += title + " " + url
+                print(INDEX,msg)
+                status = bot.send_message(chat_id="@Testesrfff", text=msg, parse_mode=telegram.ParseMode.MARKDOWN)
+            elif int(title[title.find("%")-2:title.find("%")]) > 60:
+                percent = title.find("%")
+                msg += title + " " + url
+                print(INDEX,msg)
+                status = bot.send_message(chat_id="@Testesrfff", text=msg, parse_mode=telegram.ParseMode.HTML)
+        INDEX+=1
---
+++ README.md
@@ -0,0 +1,10 @@
+<h1> Joguinhos Grátis <h1>
+
+This Telegram Bot was developed to send Game Deals to a Telegram Channel<br>
+The deals are retrieved from subreddit r/gamedeals <br>
+
+Only deals over 60% discount is shared. <br>
+
+Developed by: Rômulo Férrer Filho<br>
+Github: github.com/romulofff<br>
+Telegram: @Romulofff
