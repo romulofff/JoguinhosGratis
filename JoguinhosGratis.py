c45d14052988c279d748cda4e8100738c0bb6c28
Romulo Filho <romulo_freire_filho@hotmail.com>
2019-05-13 14:05:17 -0300

    Adicionando arquivos para deploy no Heroku

--- JoguinhosGratis.py
+++ JoguinhosGratis.py
@@ -14,7 +14,7 @@ Telegram: @Romulofff
 import praw
 import telegram 
 import os
-
+import re
 client_id = os.environ['id']
 client_secret = os.environ['secret']
 user_agent = os.environ['agent']
@@ -28,27 +28,22 @@ bot = telegram.Bot(token=telegram_token)
 reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent, username=username, password=password)
 subreddit = reddit.subreddit("gamedeals")
 
-while True:
-    new = subreddit.stream.submissions(skip_existing=True)
-    # print(new.title)
-    top = subreddit.new(limit=1)
-
-    # status = bot.send_message(chat_id="@Testesrfff", text="NOVO TESTE INICIANDO", parse_mode=telegram.ParseMode.HTML)
-    INDEX = 0
-    for submission in new:
-        
-        title = submission.title
-        url = submission.url
-        msg = ""
-        if "%" in title:
-            if title[title.find("%")-3:title.find("%")+1] == "100%":
-                msg = "*------------ESSE É GRÁTIS------------* \n"
-                msg += title + " " + url
-                print(INDEX,msg)
-                status = bot.send_message(chat_id="@Testesrfff", text=msg, parse_mode=telegram.ParseMode.MARKDOWN)
-            elif int(title[title.find("%")-2:title.find("%")]) > 60:
-                percent = title.find("%")
-                msg += title + " " + url
-                print(INDEX,msg)
-                status = bot.send_message(chat_id="@Testesrfff", text=msg, parse_mode=telegram.ParseMode.HTML)
-        INDEX+=1
+status = bot.send_message(chat_id="@Testesrfff", text="NOVO TESTE INICIANDO", parse_mode=telegram.ParseMode.HTML)
+INDEX = 0
+for submission in subreddit.stream.submissions(skip_existing=True):
+    
+    title = submission.title
+    url = submission.url
+    msg = ""
+    if "%" in title:
+        if title[title.find("%")-3:title.find("%")+1] == "100%":
+            msg = "*------------ESSE É GRÁTIS------------* \n"
+            msg += title + " " + url
+            print(INDEX,msg)
+            status = bot.send_message(chat_id="@Testesrfff", text=msg, parse_mode=telegram.ParseMode.MARKDOWN)
+        elif int(title[title.find("%")-2:title.find("%")]) > 60:
+            percent = title.find("%")
+            msg += title + " " + url
+            print(INDEX,msg)
+            status = bot.send_message(chat_id="@Testesrfff", text=msg, parse_mode=telegram.ParseMode.HTML)
+    INDEX+=1
